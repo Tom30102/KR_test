dca280b1cc362c880aae9693c69880438a465bfd
Tom30102 <procyon525@gmail.com>
2021-09-05 15:23:03 +0900

    3-1
    演習3-1の解答

--- 3-1.c
+++ 3-1.c
@@ -1,17 +1,33 @@
 #include<stdio.h>
+#include<time.h>
 
 int binsearch(int x, int v[],int n);
+int binsearch_single(int x, int v[],int n);
 
 int main(void)
 {
-    int x = 5;
-    int v[20] ={1,2,3,4,5,6,7,8,9,10};
-    int n = 10;
+    int x;
+    int v[20] ={2,3,5,7,11,13,17,23,29};
+    int n = 8;
     int a;
 
-    a = binsearch(x,v,n);
+    clock_t start_clock, end_clock;
 
-    printf("%d",a);
+
+    scanf("%d",&x);
+
+    start_clock = clock();
+    a = binsearch(x,v,n);
+    end_clock = clock();
+
+    printf("double_clock:%d\t",(double)(end_clock - start_clock));
+    printf("ans %d\n",a);
+
+    start_clock = clock();
+    a = binsearch_single(x,v,n);
+    end_clock = clock();
+    printf("single_clock:%d\t",(double)(end_clock - start_clock));
+    printf("ans %d\n",a);
 
     return 0;
 }
@@ -20,6 +36,11 @@ int main(void)
 int binsearch(int x, int v[],int n)
 {
     int low, high, mid;
+    int i;
+
+    for(i=0; i< 1000000; i++){
+        low = i;
+    }
 
     low = 0;
     high = n - 1;
@@ -40,4 +61,33 @@ int binsearch(int x, int v[],int n)
         }
     }
     return -1;
+}
+
+int binsearch_single(int x, int v[],int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+        {
+            high = mid -1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if(x == v[mid])
+    {
+        return mid;
+    }
+    else
+    {
+        return -1;
+    }
 }
