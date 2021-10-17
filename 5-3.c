8f346e2e09563833096bf7893ad3b101efcf3224
Tom30102 <procyon525@gmail.com>
2021-10-17 21:31:35 +0900

    5-3

---
+++ 5-3.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void strcat_KR(char *s, char *t);
+
+int main(void)
+{
+    char s[100];
+    char t[100];
+
+    printf("s:");
+    scanf("%s",s);
+
+    printf("t:");
+    scanf("%s",t);
+
+    printf("s:%s\tt:%s\n",s,t);
+
+    strcat_KR(s,t);
+
+    printf("s:%s\tt:%s\n",s,t);
+
+    return 0;
+}
+
+void strcat_KR(char *s, char *t)
+{
+    int i,j;
+    i = 0;
+    j = 0;
+    while(s[i] != '\0')
+    {   
+        printf("%c",s[i]);
+        i++;
+    }
+    while(t[j] != '\0')
+    {   
+        s[i] = t[j];
+        i++;
+        j++;
+        printf("$");
+    }
+
+    s[i] = '\0';
+    
+    printf("s:%s\tt:%s\n",s,t);
+}
